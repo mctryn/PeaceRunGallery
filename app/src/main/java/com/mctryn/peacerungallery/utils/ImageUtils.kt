f099a783c78f4b864041d51dc37b080505c84c6a
Vitalii Siedinkin <kurraper333@gmail.com>
2021-02-07 17:07:37 +0200

    added placeholders for glide

--- app/src/main/java/com/mctryn/peacerungallery/utils/ImageUtils.kt
+++ app/src/main/java/com/mctryn/peacerungallery/utils/ImageUtils.kt
@@ -1,9 +1,13 @@
 package com.mctryn.peacerungallery.utils
 
+import android.graphics.drawable.ColorDrawable
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
+import com.mctryn.peacerungallery.R
 
 fun ImageView.loadImageFromLink(imageLink: String) {
     Glide.with(context.applicationContext)
@@ -11,8 +15,9 @@ fun ImageView.loadImageFromLink(imageLink: String) {
         .apply(
             RequestOptions()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .dontAnimate()
                 .centerCrop()
+                .placeholder(ColorDrawable(ContextCompat.getColor(this.context, R.color.placeholder_gray)))
+                .error(ColorDrawable(ContextCompat.getColor(this.context, R.color.placeholder_gray)))
         )
         .into(this)
 }
@@ -23,7 +28,8 @@ fun ImageView.loadDetailImageFromLink(imageLink: String) {
         .apply(
             RequestOptions()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(ColorDrawable(ContextCompat.getColor(this.context, R.color.placeholder_gray)))
+                .error(ColorDrawable(ContextCompat.getColor(this.context, R.color.placeholder_gray)))
         )
         .into(this)
 }
-
